75f1ec8a7c50245821cb59adf156755190435b18
Gr33nDrag0n <git@gr33ndrag0n.com>
2017-03-26 05:04:40 -0400

    Get-Latest Initial

---
+++ Get-Latest-PoC.sh
@@ -0,0 +1,65 @@
+#------------------------------------------------------------------------------
+# Author:        Gr33nDrag0n
+# Last Update:   2017-03-26
+#
+# PoC Get Latest in BASH (For integration in ArkCommander.sh)
+#------------------------------------------------------------------------------
+
+SrcRepoList=(
+  https://snapshot.lisknode.io/     # Gr33nDrag0n
+  https://snapshot.liskwallet.net/  # isabella
+  https://lisktools.io/backups/     # MrV
+)
+
+#SrcFilenameFilter="ark_mainnet_[0-9]"
+# Debug only since Ark.io still with testnet snapshots.
+SrcFilenameFilter="ark_*_[0-9]"
+
+declare -A RemoteFile
+RemoteFile['FullPath']=''
+RemoteFile['FileName']=''
+RemoteFile['Height']=0
+
+
+read -e -r -p "$(yellow "\n Download from Ark.io? (1) ArkNode.net? (2) Seatrips.eu (3) or use Local (L) ")" -i "1" CHOICE
+if [[ "$CHOICE" =~ [1]$ ]]; then
+	echo -e "$(yellow "\n     Downloading latest snapshot from Ark.io\n")"
+	SrcRepo='https://explorer.ark.io/snapshots/'
+	RemoteFile['FileName']=$( curl -s $SrcRepo | grep -o "$SrcFilenameFilter" | sort | tail -n 1 )
+	RemoteFile['FullPath']=$( curl -s $SrcRepo | grep -o "$SrcFilenameFilter" | sort | tail -n 1 )
+	RemoteFile['Height']=$( echo ${RemoteFile['FileName']} | grep -oh "[0-9]*" )
+	
+	echo "FileName => ${RemoteFile['FileName']}"
+	echo "FullPath => ${RemoteFile['FullPath']}"
+	echo "Height => ${RemoteFile['Height']}"
+	
+#	wget -nv ${RemoteFile['FullPath']} -O $SNAPDIR/current
+#	echo -e "$(yellow "\n              Download finished\n")"
+elif [[ "$CHOICE" =~ [2]$ ]]; then
+	echo -e "$(yellow "\n     Downloading latest snapshot from ArkNode.net\n")"
+	SrcRepo='https://snapshot.arknode.net/'
+	RemoteFile['FileName']=$( curl -s $SrcRepo | grep -o "$SrcFilenameFilter" | sort | tail -n 1 )
+	RemoteFile['FullPath']=$( curl -s $SrcRepo | grep -o "$SrcFilenameFilter" | sort | tail -n 1 )
+	RemoteFile['Height']=$( echo ${RemoteFile['FileName']} | grep -oh "[0-9]*" )
+	
+	echo "FileName => ${RemoteFile['FileName']}"
+	echo "FullPath => ${RemoteFile['FullPath']}"
+	echo "Height => ${RemoteFile['Height']}"
+	
+#	wget -nv ${RemoteFile['FullPath']} -O $SNAPDIR/current
+#	echo -e "$(yellow "\n              Download finished\n")"
+elif [[ "$CHOICE" =~ [3]$ ]]; then
+	echo -e "$(yellow "\n     Downloading latest snapshot from Seatrips.eu\n")"
+	SrcRepo='https://arkexplorer.seatrips.eu/snapshots/'
+	RemoteFile['FileName']=$( curl -s $SrcRepo | grep -o "$SrcFilenameFilter" | sort | tail -n 1 )
+	RemoteFile['FullPath']=$( curl -s $SrcRepo | grep -o "$SrcFilenameFilter" | sort | tail -n 1 )
+	RemoteFile['Height']=$( echo ${RemoteFile['FileName']} | grep -oh "[0-9]*" )
+	
+	echo "FileName => ${RemoteFile['FileName']}"
+	echo "FullPath => ${RemoteFile['FullPath']}"
+	echo "Height => ${RemoteFile['Height']}"
+	
+#	wget -nv ${RemoteFile['FullPath']} -O $SNAPDIR/current
+#	echo -e "$(yellow "\n              Download finished\n")"
+fi
+
